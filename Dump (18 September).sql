8f491196b295fbbbbf0e8475f094510755383f6a
alpinnbtk <ambrosiusalvin20@gmail.com>
2025-09-18 21:04:40 +0700

    Database
    File Database ye.

---
+++ Dump (18 September).sql
@@ -0,0 +1,259 @@
+-- MySQL dump 10.13  Distrib 5.7.17, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: fullstack
+-- ------------------------------------------------------
+-- Server version	5.5.5-10.4.32-MariaDB
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `akun`
+--
+
+DROP TABLE IF EXISTS `akun`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `akun` (
+  `username` varchar(20) NOT NULL,
+  `password` varchar(100) DEFAULT NULL,
+  `nrp_mahasiswa` char(9) DEFAULT NULL,
+  `npk_dosen` char(6) DEFAULT NULL,
+  `isadmin` tinyint(1) DEFAULT 0,
+  PRIMARY KEY (`username`),
+  KEY `fk_akun_mahasiswa_idx` (`nrp_mahasiswa`),
+  KEY `fk_akun_dosen1_idx` (`npk_dosen`),
+  CONSTRAINT `fk_akun_dosen1` FOREIGN KEY (`npk_dosen`) REFERENCES `dosen` (`npk`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `fk_akun_mahasiswa` FOREIGN KEY (`nrp_mahasiswa`) REFERENCES `mahasiswa` (`nrp`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `akun`
+--
+
+LOCK TABLES `akun` WRITE;
+/*!40000 ALTER TABLE `akun` DISABLE KEYS */;
+INSERT INTO `akun` VALUES ('alpinnbtk','test','160423055','197030',1);
+/*!40000 ALTER TABLE `akun` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `chat`
+--
+
+DROP TABLE IF EXISTS `chat`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `chat` (
+  `idchat` int(11) NOT NULL AUTO_INCREMENT,
+  `idthread` int(11) NOT NULL,
+  `username_pembuat` varchar(20) NOT NULL,
+  `isi` text DEFAULT NULL,
+  `tanggal_pembuatan` datetime DEFAULT NULL,
+  PRIMARY KEY (`idchat`),
+  KEY `fk_chat_thread1_idx` (`idthread`),
+  KEY `fk_chat_akun1_idx` (`username_pembuat`),
+  CONSTRAINT `fk_chat_akun1` FOREIGN KEY (`username_pembuat`) REFERENCES `akun` (`username`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `fk_chat_thread1` FOREIGN KEY (`idthread`) REFERENCES `thread` (`idthread`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `chat`
+--
+
+LOCK TABLES `chat` WRITE;
+/*!40000 ALTER TABLE `chat` DISABLE KEYS */;
+/*!40000 ALTER TABLE `chat` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `dosen`
+--
+
+DROP TABLE IF EXISTS `dosen`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `dosen` (
+  `npk` char(6) NOT NULL,
+  `nama` varchar(45) DEFAULT NULL,
+  `foto_extension` varchar(4) DEFAULT NULL,
+  PRIMARY KEY (`npk`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `dosen`
+--
+
+LOCK TABLES `dosen` WRITE;
+/*!40000 ALTER TABLE `dosen` DISABLE KEYS */;
+INSERT INTO `dosen` VALUES ('197030','Dr. Susana Limanto, M.Si.','jpg'),('206020','Dr. Liliana',NULL),('208020','Dr. Andre',NULL),('217003','Vincentius Riandaru Prasetyo, M.Cs.','jpg');
+/*!40000 ALTER TABLE `dosen` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `event`
+--
+
+DROP TABLE IF EXISTS `event`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `event` (
+  `idevent` int(11) NOT NULL AUTO_INCREMENT,
+  `idgrup` int(11) NOT NULL,
+  `judul` varchar(45) DEFAULT NULL,
+  `judul-slug` varchar(45) DEFAULT NULL,
+  `tanggal` datetime DEFAULT NULL,
+  `keterangan` text DEFAULT NULL,
+  `jenis` enum('Privat','Publik') DEFAULT NULL,
+  `poster_extension` varchar(4) DEFAULT NULL,
+  PRIMARY KEY (`idevent`),
+  KEY `fk_event_grup1_idx` (`idgrup`),
+  CONSTRAINT `fk_event_grup1` FOREIGN KEY (`idgrup`) REFERENCES `grup` (`idgrup`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `event`
+--
+
+LOCK TABLES `event` WRITE;
+/*!40000 ALTER TABLE `event` DISABLE KEYS */;
+/*!40000 ALTER TABLE `event` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `grup`
+--
+
+DROP TABLE IF EXISTS `grup`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `grup` (
+  `idgrup` int(11) NOT NULL AUTO_INCREMENT,
+  `username_pembuat` varchar(20) NOT NULL,
+  `nama` varchar(45) DEFAULT NULL,
+  `deskripsi` varchar(45) DEFAULT NULL,
+  `tanggal_pembentukan` datetime DEFAULT NULL,
+  `jenis` enum('Privat','Publik') DEFAULT NULL,
+  `kode_pendaftaran` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`idgrup`),
+  KEY `fk_grup_akun1_idx` (`username_pembuat`),
+  CONSTRAINT `fk_grup_akun1` FOREIGN KEY (`username_pembuat`) REFERENCES `akun` (`username`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `grup`
+--
+
+LOCK TABLES `grup` WRITE;
+/*!40000 ALTER TABLE `grup` DISABLE KEYS */;
+/*!40000 ALTER TABLE `grup` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `mahasiswa`
+--
+
+DROP TABLE IF EXISTS `mahasiswa`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `mahasiswa` (
+  `nrp` char(9) NOT NULL,
+  `nama` varchar(45) DEFAULT NULL,
+  `gender` enum('Pria','Wanita') DEFAULT NULL,
+  `tanggal_lahir` date DEFAULT NULL,
+  `angkatan` year(4) DEFAULT NULL,
+  `foto_extention` varchar(4) DEFAULT NULL,
+  PRIMARY KEY (`nrp`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `mahasiswa`
+--
+
+LOCK TABLES `mahasiswa` WRITE;
+/*!40000 ALTER TABLE `mahasiswa` DISABLE KEYS */;
+INSERT INTO `mahasiswa` VALUES ('160423055','Alvin Kurniawan','Pria','2005-10-20',2023,'jpg'),('160423078','Richie Viriyananda Hartono','Pria','2005-10-20',2023,NULL),('160423088','Ericko Sutanto','Pria','2005-10-20',2023,NULL),('160423095','Jonathan Andrian Hadinata','Pria','2005-09-30',2025,'');
+/*!40000 ALTER TABLE `mahasiswa` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `member_grup`
+--
+
+DROP TABLE IF EXISTS `member_grup`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `member_grup` (
+  `idgrup` int(11) NOT NULL,
+  `username` varchar(20) NOT NULL,
+  PRIMARY KEY (`idgrup`,`username`),
+  KEY `fk_grup_has_akun_akun1_idx` (`username`),
+  KEY `fk_grup_has_akun_grup1_idx` (`idgrup`),
+  CONSTRAINT `fk_grup_has_akun_akun1` FOREIGN KEY (`username`) REFERENCES `akun` (`username`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `fk_grup_has_akun_grup1` FOREIGN KEY (`idgrup`) REFERENCES `grup` (`idgrup`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `member_grup`
+--
+
+LOCK TABLES `member_grup` WRITE;
+/*!40000 ALTER TABLE `member_grup` DISABLE KEYS */;
+/*!40000 ALTER TABLE `member_grup` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `thread`
+--
+
+DROP TABLE IF EXISTS `thread`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `thread` (
+  `idthread` int(11) NOT NULL AUTO_INCREMENT,
+  `username_pembuat` varchar(20) NOT NULL,
+  `idgrup` int(11) NOT NULL,
+  `tanggal_pembuatan` datetime DEFAULT NULL,
+  `status` enum('Open','Close') DEFAULT 'Open',
+  PRIMARY KEY (`idthread`),
+  KEY `fk_thread_akun1_idx` (`username_pembuat`),
+  KEY `fk_thread_grup1_idx` (`idgrup`),
+  CONSTRAINT `fk_thread_akun1` FOREIGN KEY (`username_pembuat`) REFERENCES `akun` (`username`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `fk_thread_grup1` FOREIGN KEY (`idgrup`) REFERENCES `grup` (`idgrup`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `thread`
+--
+
+LOCK TABLES `thread` WRITE;
+/*!40000 ALTER TABLE `thread` DISABLE KEYS */;
+/*!40000 ALTER TABLE `thread` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2025-09-18 21:03:54
