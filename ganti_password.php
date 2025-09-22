890fa898da802719f6e895f64820ee287448c6ed
LarzQ <penyamar777@gmail.com>
2025-09-22 22:19:37 +0700

    Update ganti_password.php

--- ganti_password.php
+++ ganti_password.php
@@ -13,7 +13,7 @@ if ($mysqli->connect_errno) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Password</title>
 </head>
 
 <body>
